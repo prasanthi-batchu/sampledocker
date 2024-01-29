2a044f46df36bd7898c85072978ad203b6d11079
prasanthi-batchu <prasanthi_batchu@arawinz.com>
2024-01-29 11:02:09 +0530

    add new route

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ def hello_world():
    return 'Hello World..'
 
 
+@app.route('/test_route')
+def test():
+   return 'Hello this is test route..'
+
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
