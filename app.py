da4da46ded3248183eeb81c401010b9f5dcf6775
prasanthi-batchu <prasanthi_batchu@arawinz.com>
2024-01-29 15:43:57 +0530

    add

--- app.py
+++ app.py
@@ -8,13 +8,10 @@ def hello_world():
    return 'Hello World..'
 
 
-# @app.route('/test_route')
-# def test():
-#    return 'Hello this is test route..'
+@app.route('/test_route')
+def test():
+   return 'Hello this is test route..'
 
-@app.route('/new_route')
-def new_route():
-   return 'Hello this is new route..'
 
 
 
