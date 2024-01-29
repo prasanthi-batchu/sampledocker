a9f309f0b87e9270b8f695c0a0ea0dc77e85b6ff
prasanthi-batchu <prasanthi_batchu@arawinz.com>
2024-01-29 11:40:43 +0530

    remove route

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def hello_world():
    return 'Hello World..'
 
 
-@app.route('/test_route')
-def test():
-   return 'Hello this is test route..'
+# @app.route('/test_route')
+# def test():
+#    return 'Hello this is test route..'
 
 @app.route('/new_route')
 def new_route():
